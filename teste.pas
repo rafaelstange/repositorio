1bc398768ef54b9104c124c6111f0459925be61b
rafaelstange <rafael.stange@royalsoft.com.br>
2017-06-02 13:57:01 -0300

    Merge branch 'master' into teste_alteracao

--- teste.pas
+++ teste.pas
@@ -4,6 +4,26 @@ interface
 
 uses Windows, System, Controls, DB, SysUtils;
 
+type
+  TPessoa = class
+  private 
+    FID: integer;
+  public
+    property ID: integer read FID write FID;
+    constructor Create;
+    destructor Destroy; override;	
+  end;
+
 implementation
 
+procedure constructor Create;
+begin
+  
+end;
+
+procedure Destructor Destroy; override;
+begin
+ 
+  inherited;
+end;
 end;
